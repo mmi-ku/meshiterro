2ead7d71126deecf7771357b5810ad02b19ec4ec
mmi-ku <161840539+mmi-ku@users.noreply.github.com>
2024-05-02 07:21:48 +0000

    19章

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get 'homes/about', to: 'homes#about', as: 'about'
 
-    resources :post_images, only: [:new, :create, :index, :show, :destroy]
-    resources :users, only: [:show, :edit]
-
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+        resource :favorite, only: [:create, :destroy]
+        resources :post_comments, only: [:create, :destory]
+    end
+    
+    resources :users, only: [:show, :edit, :update]
 end
